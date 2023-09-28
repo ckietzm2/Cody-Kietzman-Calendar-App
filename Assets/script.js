175c3e4972696d8d3ffb3be3cbb012d29e71bd16
Cody K <ckietzm2@gmail.com>
2023-09-28 13:07:43 -0500

    Added button function and saves in local storage.  Also updated folders and directories

--- Assets/script.js
+++ Assets/script.js
@@ -1,36 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
-var timerP = $('#currentDay')
-var startDay = dayjs().format('MMM D, YYYY');
-var startTime = dayjs().format('hh : mm : ss');
+var timerPage = $('#currentDay')
+var currentDay = dayjs().format('MMM D, YYYY');
+var currentTime = dayjs().format('hh : mm : ss');
 var curHour = dayjs().format('H')
 
+// Sets all variables to military time to compare later
 
 var hourNine = document.querySelector('#hour-9')
 var hourNineValue = 9
+
 
 var hourTen = document.querySelector('#hour-10')
 var hourTenValue = 10
@@ -57,21 +35,21 @@ var hourFive = document.querySelector('#hour-5')
 var hourFiveValue = 17
 
 
+// Shows time on top of page
+timerPage.text(currentDay + ' at ' + currentTime);
 
 
-
-timerP.text(startDay + ' at ' + startTime);
-
+// Run time to that it is constantly updating
 function timerStart() {
   setInterval(function() {
     var today = dayjs().format('MMM D, YYYY');
     var time = dayjs().format('hh : mm : ss');
-    timerP.text(today + ' at ' + time);
+    timerPage.text(today + ' at ' + time);
   }, 1000);
   
  
 }
-
+// Checks if current hour is before, current, or after hour on time block.  I then set the class to the corresponding outcome
 function checkTime() {
 
   if (curHour<hourNineValue){
@@ -159,9 +137,62 @@ if (curHour<hourFiveValue){
 
 }
 
+// Created button from class in HTML
+
+var saveBtn = $('.btn')
+var storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 
-timerStart()
+// when button is clicked it saves text by targeting the parents of button in HTML
+saveBtn.on('click', function (event) {
+  event.stopPropagation();
+
+  var btnClicked = $(event.target);
+  var parent = btnClicked.parent().parent()
+  var parentId = btnClicked.parent().parent().attr('id')
+  
+  // If id or time block area is undefined, it sets the ID to correct text area
+  if (parentId === undefined) {
+    parentId = btnClicked.parent().attr('id')
+    parent = btnClicked.parent()
+  }
+
+// Creates object to allow to get from local storage
+
+  var task = parent.children().eq(1).val() || " ";
+
+  var taskObj = {
+    task: task,
+    parent: parentId,
+  };  
+
+  // Loops through to check text area per ID and then splices the tasks
+
+  for (i=0; i < storedTasks.length; i++ ) {
+    if (taskObj.parent === storedTasks[i].parent) {
+      storedTasks.splice(i, 1)
+    }
+  }
+//  Pushes new task to keep storage
+  storedTasks.push((taskObj));
+  localStorage.setItem("tasks", (JSON.stringify(storedTasks)));
+    
+  memory()
+});
+// reloads all of the currently stored tasks upon broswer opening
+function memory() {
+  for (i=0; i < storedTasks.length; i++ ) {
+    var id = storedTasks[i].parent
+    var text = storedTasks[i].task
+    $('#' + id).children().eq(1).val(text)
+  }
+  console.log(storedTasks);
+
+
+}
+
+
+memory()
 checkTime()
-
+timerStart()
 
